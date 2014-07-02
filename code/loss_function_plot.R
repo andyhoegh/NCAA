c829c8e723fb1743ef13de94d5137e9e3b84f10d
Lucas Roberts <rlucas7@vt.edu>
2014-07-02 14:32:58 -0400

    added the updated loss function into luck section and changes all instances of scoring to loss in luck section

--- code/loss_function_plot.R
+++ code/loss_function_plot.R
@@ -6,31 +6,30 @@ x<-seq(0,1,by=0.01)
 #l2<- - log(1-abs(.5-x))
 l3<- - log(1-abs(1-x))
 l4<- - log(1-abs(0-x))
-l<-(l3+l4)/2 -log(2)
-l5<- -log(x) -log(1-x) 
+l5<- -log(1-abs(0.5-x))
 # l5<- -log(1-x) # this equals, l4, the case of eqn (8) with y_i==0
 # l6<- -abs(.5-x)*log(abs(.5-x)) # don't use this one, 
 #plot the function versus p to see why
 
 pdf(file='loss_function_plot_col.pdf')
-plot(x,l,type='l', xlab='p', ylab='score',ylim=c(0,5))
+plot(x,l3,type='l', xlab='p', ylab='score',ylim=c(0,5))
 #abline(h=0)
 #lines(x,l2,col='green')
-#lines(x,l4,col='red')
-lines(x,l5,col='red')
+lines(x,l4,col='red')
+lines(x,l5,col='blue')
 #lines(x,l5,col='purple') 
-legend(.5,3.5,c('Kaggle Loss','Proposed Loss'), lty=c(1,1), lwd=c(2.5,2.5),col=c("red",'black'))
+legend(.5,3.5,c('Part 1','Part 2','Addition Proposed'), lty=c(1,1), lwd=c(2.5,2.5),col=c("black",'red','blue'))
 dev.off()
 
 
 pdf(file='loss_function_plot_bw.pdf')
-plot(x,l,type='l', xlab='p', ylab='score',ylim=c(0,5))
+plot(x,l3,type='l', xlab='p', ylab='score',ylim=c(0,5))
 #abline(h=0)
 #lines(x,l2,col='green')
 #lines(x,l4,col='red')
-lines(x,l5,lty=2)
-#lines(x,l5,col='purple') 
-legend(.5,3.5,c('Kaggle Loss','Proposed Loss'), lty=c(2,1), lwd=c(2.5,2.5))
+lines(x,l4,lty=2)
+lines(x,l5,lty=3) 
+legend(.5,3.5,c('Part 1','Part 2','Addition Proposed'), lty=c(1,2,3), lwd=c(2.5,2.5))
 dev.off()
 
 
